450d665f5d125ad9a434b19e14461570125dd640
Ahadi <ahadic044@gmail.com>
2024-03-05 14:09:31 +0200

    making the whole file three lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # a script that displays the response bondy length from a url
-
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-link=$1
-header_response=$(curl -sI "$link")
-echo "$header_response" | grep -i '^Content-length:' | awk '{print $2}' | tr -d "\r"
+curl -sI "$1" | grep -i '^Content-length' | awk '{print $2}'
